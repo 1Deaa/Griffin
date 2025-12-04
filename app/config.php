4ae54a35248def822da2d031b4a0b1ce495724d0
drahwanj <drahwanj@student.42amman.com>
2025-12-04 22:49:31 +0300

    removed comment containing credinitals.

--- app/config.php
+++ app/config.php
@@ -5,11 +5,6 @@ $DB_USER = getenv('DB_USER');
 $DB_PASS = getenv('DB_PASS');
 $DB_NAME = getenv('DB_NAME');
 
-// $DB_HOST = "griffin-database";
-// $DB_USER = "griffinuser";
-// $DB_PASS = "griffin123";
-// $DB_NAME = "griffindb";
-
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 
 if ($mysqli->connect_errno)
