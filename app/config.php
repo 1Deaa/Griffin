e79b28aa612a5f30266f7865ad945b1ea7b3a938
drahwanj <drahwanj@student.42amman.com>
2025-12-05 23:25:46 +0300

    changing indentation to tabs instead of spaces

--- app/config.php
+++ app/config.php
@@ -9,7 +9,7 @@ $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 
 if ($mysqli->connect_errno)
 {
-    die("Database connection failed: " . $mysqli->connect_error);
+	die("Database connection failed: " . $mysqli->connect_error);
 }
 
 ?>
